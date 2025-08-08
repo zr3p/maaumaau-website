5991611faa2c0392c9fcb0f3cfd73a324b30019a
zr3p <signal@vakne.eu>
2025-08-08 23:36:30 +0100

    using DS managed to moce Footer to standard TW config and should adapt to dark and light mode when resolved

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,10 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"], // ✅ Tells Tailwind to scan Astro + JSX files for classes
   theme: {
     extend: {}, // ✅ Placeholder to add future custom styles if needed
+    colors: {
+      'footer-bg': { DEFAULT: '#000', dark: '#000' },
+      'footer-text': { DEFAULT: '#FFF', dark: '#FFF' }
+    }
   },
   plugins: [typography], // ✅ Enables better spacing/styling for content like privacy, terms, blog posts
 };
